7fdc006429937decdf8be7c6f6ecd437a1fb9ce0
ELCL <janekwaugh81@gmail.com>
2026-05-06 11:05:28 -0500

    Initial commit

---
+++ wezterm.lua
@@ -0,0 +1,32 @@
+-- Pull in the wezterm API
+local wezterm = require('wezterm')
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.cursor_blink_ease_in = "Constant"
+config.cursor_blink_ease_out = "Constant"
+config.animation_fps = 60
+
+config.color_scheme = 'nord'
+config.colors = {
+    background = '#171717',
+    cursor_bg = '#C4D8E2',
+    cursor_border = '#C4D8E2',
+}
+
+config.font = wezterm.font('JetBrainsMono Nerd Font Mono', {weight = 'DemiBold'})
+config.font_size = 13.0
+
+config.enable_tab_bar = false
+config.window_background_opacity = 1.0
+config.window_padding = {
+    left = '1cell',
+    right = '1cell',
+    top = '1cell',
+    bottom = '0.5cell',
+}
+
+config.enable_wayland = false
+
+return config
